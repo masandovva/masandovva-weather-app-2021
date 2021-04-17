4e0988c6fb7daf8a733859746362ac07bbef2310
masandovva <masandovva@gmail.com>
2021-04-16 21:00:30 -0500

    Remove console

--- index.js
+++ index.js
@@ -96,7 +96,6 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   let apiKey = "0d40b598540d71d8dcaf321e85237ab2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
